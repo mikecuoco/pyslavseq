0bbaa2a1cb628be13c09075926a4ef3e059552d9
mikecuoco <mcuoco@ucsd.edu>
2022-07-19 13:15:33 -0700

    simple solution

--- tests/tags/test_add_tags_perl.py
+++ tests/tags/test_add_tags_perl.py
@@ -7,49 +7,14 @@ import pysam
 import pytest
 
 # setup class for testing
-class TestAddTagsPerl:
-    def __init__(self):
-        from pyslavseq.tags.add_tags import setup_aligner, set_parameters
-        self.aligner = setup_aligner()
-        self.parameters = set_parameters()
+from pyslavseq.tags.add_tags import setup_aligner, set_parameters
+aligner = setup_aligner()
+CONSENSUS, PREFIX_LENGTH, R1_FLANK_LENGTH, R2_FLANK_LENGTH, SOFT_CLIP_LENGTH_THRESHOLD = set_parameters()
 
-    def bam(self, bam_file):
-        self.bam = pysam.AlignmentFile(str(bam_file), 'rb')
-
-    def ref(self,ref_file):
-        self.ref = pyfaidx.Fasta(str(ref_file))
-
-# define fixture 
-@pytest.fixture
-def test():
-    test = TestAddTagsPerl()
-    test.bam(bam_file = Path(__file__).parent / 'test.bam')
-    test.ref(ref_file = Path(__file__).parent / "hs37d5_hg19_chr22.fa")
-    return test
-
-@pytest.fixture
-def read(test):
-    read_id = ""
-    for count, read in enumerate(test.bam):  
-        # skip secondary and supplementary alignments
-        if read.is_secondary or read.is_supplementary: continue
-        print(f"Processing read #{count+1} with id: {read.query_name}")
-        if read.is_read1:
-            r1 = read
-        else:
-            r2 = read
-
-        # check if read is a mate of the previous read
-        if read.query_name != read_id:
-            read_id = read.query_name
-            continue
-
-        # calculate YR and YS if read has cigarstring
-        if r2.cigarstring:
-            yield r2
-
-    test.bam.close()
-
+ref_file = Path(__file__).parent / "hs37d5_hg19_chr22.fa"
+bam_file = Path(__file__).parent / 'test.bam'
+bam = pysam.AlignmentFile(str(bam_file), 'rb')
+ref = pyfaidx.Fasta(str(ref_file))
 
 def test_calculate_yr(read, SOFT_CLIP_LENGTH_THRESHOLD):
     from pyslavseq.tags.add_tags import calculate_yr
@@ -58,5 +23,27 @@ def test_calculate_yr(read, SOFT_CLIP_LENGTH_THRESHOLD):
 
 def test_calculate_ys(read):
     from pyslavseq.tags.add_tags import calculate_ys
-    yr = calculate_ys(read)
-    assert int(yr) == read.get_tag('YR')
+    ys = calculate_ys(read)
+    assert int(ys) == read.get_tag('YS')
+
+read_id = ""
+for count, read in enumerate(bam):  
+    # skip secondary and supplementary alignments
+    if read.is_secondary or read.is_supplementary: continue
+    print(f"Processing read #{count+1} with id: {read.query_name}")
+    if read.is_read1:
+        r1 = read
+    else:
+        r2 = read
+
+    # check if read is a mate of the previous read
+    if read.query_name != read_id:
+        read_id = read.query_name
+        continue
+
+    # calculate YR and YS if read has cigarstring
+    if r2.cigarstring:
+        test_calculate_yr(r2, SOFT_CLIP_LENGTH_THRESHOLD)
+        test_calculate_ys(r2)
+
+
